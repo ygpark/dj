85f7445bd82786a0562353037064e5bd4e2e6ca3
Young-Gi Park <ghostyak@gmail.com>
2014-02-23 16:32:53 +0900

    reindent

--- dj.sh
+++ dj.sh
@@ -44,7 +44,7 @@ function dj_ctrl_up
     dj_prev
 
     # print current dir list
-	echo "[ Directory Jump ]"
+    echo "[ Directory Jump ]"
     echo
     dj_dirs
     
@@ -62,7 +62,7 @@ function dj_ctrl_down
     dj_next
 
     # print current dir list
-	echo "[ Directory Jump ]"
+    echo "[ Directory Jump ]"
     echo
     dj_dirs
 
@@ -89,12 +89,12 @@ function dj_ctrl_left
     cd ..
 
     # print current dir
-	echo "[ Directory Jump ]"
+    echo    "[ Directory Jump ]"
     echo
-    echo "    TOP: `head -n1 $DJ_FNAME_STACK`"
+    echo    "    TOP: `head -n1 $DJ_FNAME_STACK`"
     echo -e "    PWD: \033[7m$PWD\033[27m"
     echo
-	echo "(See 'dj help' for for information)"
+    echo    "(See 'dj help' for for information)"
     echo
 
     # delete last history
@@ -103,39 +103,41 @@ function dj_ctrl_left
 
 function dj_ctrl_right
 {
-    clear
+    clear;
     if [ -e $DJ_FNAME_STACK ]; then
+
         # go up
-        cd $(tail -n1 $DJ_FNAME_STACK)
+        cd $(tail -n1 $DJ_FNAME_STACK);
 
         # pop stack
-        dj_pop_dir_from_stack
+        dj_pop_dir_from_stack;
     fi
 
     # print current dir
-	echo "[ Directory Jump ]"
+    echo "[ Directory Jump ]"
     echo
     if [ -e $DJ_FNAME_STACK ]; then
-        echo "    TOP: `head -n1 $DJ_FNAME_STACK`"
+        echo "    TOP: `head -n1 $DJ_FNAME_STACK`";
     else
-        echo "    TOP: $PWD"
+        echo "    TOP: $PWD";
     fi
-    echo -e "    PWD: \033[7m$PWD\033[27m"
-    echo
-	echo "(See 'dj help' for for information)"
-    echo
+    echo -e "    PWD: \033[7m$PWD\033[27m";
+    echo;
+    echo "(See 'dj help' for for information)";
+    echo;
 
     # delete last history
-    history -d $(($HISTCMD-1))
+    history -d $(($HISTCMD-1));
 }
 
 
 # Brief: 
-function dj_push_dir_into_stack {
+function dj_push_dir_into_stack
+{
     if [ -e $DJ_FNAME_STACK ] \
         && [ $(tail -n1 $DJ_FNAME_STACK) != $PWD ] \
         && [ $PWD != "/" ]
-    then
+        then
         echo $PWD >> $DJ_FNAME_STACK
     elif ! [ -e $DJ_FNAME_STACK ]; then
         echo $PWD >> $DJ_FNAME_STACK
@@ -143,362 +145,367 @@ function dj_push_dir_into_stack {
 }
 
 function dj_pop_dir_from_stack {
-    [ -e $DJ_FNAME_STACK ] && sed -i '$ d' $DJ_FNAME_STACK
-    ! [ -s $DJ_FNAME_STACK ] && rm $DJ_FNAME_STACK
+	[ -e $DJ_FNAME_STACK ] && sed -i '$ d' $DJ_FNAME_STACK
+	! [ -s $DJ_FNAME_STACK ] && rm $DJ_FNAME_STACK
 }
 
 function dj_print_usage
 {
-    echo "dj - Directory Jump"
-    echo
-    echo "Usage: "
-    echo "	dj                 : print directories"
-    echo "	dj [index]         : change directory by index"
-    echo
-    echo "	dj add             : add current directory"
-    echo "	dj add [dir]       : add directory"
-    echo
-    echo "	dj rm              : remove current directory"
-    echo "	dj rm [index]      : remove directory by index"
-    echo
-    echo "	dj save [filename] : save dir list into the file (default: $DJ_FNAME_SAVE)"
-    echo
-    echo "	dj load [filename] : load dir list from the file (default: $DJ_FNAME_SAVE)"
-    echo
-    echo "	dj clean           : clean the stack"
-    echo
-    echo "	dj help          : print usage"
-    echo
-    echo "Key Map:"
-    echo "       CTRL + UP          : display stored directoris"
-    echo "       CTRL + LEFT        : change previous directory and display "
-    echo "       CTRL + RIGHT       : change next directory and display"
+    echo "dj - Directory Jump";
+    echo;
+    echo "Usage: ";
+    echo "	dj                 : print directories";
+    echo "	dj [index]         : change directory by index";
+    echo;
+    echo "	dj add             : add current directory";
+    echo "	dj add [dir]       : add directory";
+    echo;
+    echo "	dj rm              : remove current directory";
+    echo "	dj rm [index]      : remove directory by index";
+    echo;
+    echo "	dj save [filename] : save dir list into the file (default: $DJ_FNAME_SAVE)";
+    echo;
+    echo "	dj load [filename] : load dir list from the file (default: $DJ_FNAME_SAVE)";
+    echo;
+    echo "	dj clean           : clean the stack";
+    echo;
+    echo "	dj help          : print usage";
+    echo;
+    echo "Key Map:";
+    echo "       CTRL + UP          : display stored directoris";
+    echo "       CTRL + LEFT        : change previous directory and display ";
+    echo "       CTRL + RIGHT       : change next directory and display";
 }
 
 function dj_filter_real_path
 {
-	rm -f $DJ_FNAME_TEMP 
-	touch $DJ_FNAME_TEMP
+	rm -f $DJ_FNAME_TEMP;
+	touch $DJ_FNAME_TEMP;
 
 	while read line;
 	do
 		if [ -d $line ]; then
-			echo "$line" >> $DJ_FNAME_TEMP
+			echo "$line" >> $DJ_FNAME_TEMP;
 		fi
 	done < <(cat $DJ_FNAME_ABS)
 
-	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS
+	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS;
 }
 
 function dj_dirs
 {
-	LINE_COUNT=$(wc -l $DJ_FNAME_REL | awk '{print $1}')
+	LINE_COUNT=$(wc -l $DJ_FNAME_REL | awk '{print $1}');
 	if [ "$LINE_COUNT" == "0" ]; then
-		echo "(empty stack. 'dj --help')"
+		echo "(empty stack. 'dj --help')";
 		return;
 	fi
 
-	dj_filter_real_path
-	dj_make_short_path
+	dj_filter_real_path;
+	dj_make_short_path;
 
 	while read line;
 	do
-		directory=$(echo $line | awk '{print $2}')
+		directory=$(echo $line | awk '{print $2}');
 
 		#DJ_LONG_PATH=$(echo "$directory" | sed "s/^~/$DJ_HOME/")
 
 		#Point out current directory
 		if [[ $PWD == $directory ]];then
-			echo -e "    \033[7m$line\033[27m"
+			echo -e "    \033[7m$line\033[27m";
 		else
 			echo "    $line"
 		fi
 	done < <(cat -n $DJ_FNAME_ABS)
 
-	echo 
-	echo "(See 'dj help' for for information)"
-	echo 
+	echo;
+	echo "(See 'dj help' for for information)";
+	echo;
 }
 
 function dj_add
 {
-	# Check param
-	if ! test -d "$1"; then
-		echo "error: Invalid parameter" 1>&2
-		return 1;
-	fi
+    # Check param
+    if ! test -d "$1"; then
+        echo "error: Invalid parameter" 1>&2
+        return 1;
+    fi
 
-	echo $(readlink -f $1) >> $DJ_FNAME_ABS
-	dj_filter_real_path
+    echo $(readlink -f $1) >> $DJ_FNAME_ABS;
+    dj_filter_real_path;
 
-	sort -u $DJ_FNAME_ABS -o $DJ_FNAME_TEMP 
-	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS
+    sort -u $DJ_FNAME_ABS -o $DJ_FNAME_TEMP;
+    mv $DJ_FNAME_TEMP $DJ_FNAME_ABS;
 
-	dj_make_short_path
-	return;
+    dj_make_short_path;
+    return;
 }
 
 function dj_clean
 {
-	rm -f $DJ_FNAME_REL
-	touch $DJ_FNAME_REL
+    rm -f $DJ_FNAME_REL;
+    touch $DJ_FNAME_REL;
 }
- 
+
 function dj_save
 {
-	dj_filter_real_path
+    dj_filter_real_path;
 
-	if [ "$1" = "" ]; then
-		cp $DJ_FNAME_ABS $DJ_FNAME_SAVE
-		return 0;
-	else 
-		cp $DJ_FNAME_ABS $1
-		return 0;
-	fi
+    if [ "$1" = "" ]; then
+        cp $DJ_FNAME_ABS $DJ_FNAME_SAVE;
+        return 0;
+    else 
+        cp $DJ_FNAME_ABS $1;
+        return 0;
+    fi
 }
- 
+
 function dj_load
 {
-	rm -f $DJ_FNAME_TEMP 
-	touch $DJ_FNAME_TEMP
+    rm -f $DJ_FNAME_TEMP;
+    touch $DJ_FNAME_TEMP;
 
-	if test -f "$1" ; then
-		cp $1 $DJ_FNAME_TEMP
+    if test -f "$1" ; then
+        cp $1 $DJ_FNAME_TEMP;
+
         #to support the old version
-        sed -i "s/^~/$DJ_HOME/" $DJ_FNAME_TEMP
-		cp $DJ_FNAME_TEMP $DJ_FNAME_ABS
-	else
-		cp $DJ_FNAME_SAVE $DJ_FNAME_ABS
-	fi
+        sed -i "s/^~/$DJ_HOME/" $DJ_FNAME_TEMP;
+        cp $DJ_FNAME_TEMP $DJ_FNAME_ABS;
+    else
+        cp $DJ_FNAME_SAVE $DJ_FNAME_ABS;
+    fi
 
-	dj_filter_real_path
-	dj_make_short_path
+    dj_filter_real_path;
+    dj_make_short_path;
 
     return 0;
 }
 
 function dj_make_short_path
 {
-	if ! test -f "$DJ_FNAME_HASH" 
-	then
-		sha1sum $DJ_FNAME_ABS > $DJ_FNAME_HASH
-	fi
+    if ! test -f "$DJ_FNAME_HASH"; then
+        sha1sum $DJ_FNAME_ABS > $DJ_FNAME_HASH;
+    fi
 
-	if [[ "$(cat $DJ_FNAME_HASH)" == $(sha1sum $DJ_FNAME_ABS) ]]
-	then
-		return
-	else
-		cp $DJ_FNAME_ABS $DJ_FNAME_REL
-		sed -i "s/^$DJ_HOME/~/" $DJ_FNAME_REL
-		sha1sum $DJ_FNAME_ABS > $DJ_FNAME_HASH
-	fi
+    if [[ "$(cat $DJ_FNAME_HASH)" == $(sha1sum $DJ_FNAME_ABS) ]]; then
+        return;
+    else
+        cp $DJ_FNAME_ABS $DJ_FNAME_REL;
+        sed -i "s/^$DJ_HOME/~/" $DJ_FNAME_REL;
+        sha1sum $DJ_FNAME_ABS > $DJ_FNAME_HASH;
+    fi
 
-	return 0;
+    return 0;
 }
 
- 
+
 function dj_rm
 {
-	# Check argument whether number.
-	if [ ! $(echo $1 | sed -n '/^[0-9]\+$/p') ]; then
-		echo "error: Input number" 1>&2
-		dj_print_usage;
-		return;
-	fi
+    # Check argument whether number.
+    if [ ! $(echo $1 | sed -n '/^[0-9]\+$/p') ]; then
+        echo "error: Input number" 1>&2
+        dj_print_usage;
+        return;
+    fi
 
-	dj_filter_real_path
+    dj_filter_real_path;
 
-	rm -f $DJ_FNAME_TEMP 
-	touch $DJ_FNAME_TEMP
+    rm -f $DJ_FNAME_TEMP;
+    touch $DJ_FNAME_TEMP;
 
-	while read line;
-	do
-		index=$(echo $line | awk '{print $1}')
-		directory=$(echo $line | awk '{print $2}')
+    while read line;
+    do
+        index=$(echo $line | awk '{print $1}');
+        directory=$(echo $line | awk '{print $2}');
 
-		if [ "$1" -ne "$index" ];then
-			echo $directory >> $DJ_FNAME_TEMP
-		fi
+        if [ "$1" -ne "$index" ];then
+            echo $directory >> $DJ_FNAME_TEMP;
+        fi
 
-	done < <(cat -n $DJ_FNAME_ABS)
+    done < <(cat -n $DJ_FNAME_ABS)
 
-	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS
-	dj_make_short_path
+    mv $DJ_FNAME_TEMP $DJ_FNAME_ABS;
+    dj_make_short_path;
 
-	return 0;
+    return 0;
 }
 
 function dj_rm_by_dirname
 {
-	rm -f $DJ_FNAME_TEMP
-	touch $DJ_FNAME_TEMP
+	rm -f $DJ_FNAME_TEMP;
+	touch $DJ_FNAME_TEMP;
 
 	while read line;
 	do
-		if [[ ! "$line" == "$(readlink -f $1)" ]]
-		then
-			echo "$line" >> $DJ_FNAME_TEMP
+		if [[ ! "$line" == "$(readlink -f $1)" ]]; then
+			echo "$line" >> $DJ_FNAME_TEMP;
 		fi
 	done < <(cat $DJ_FNAME_ABS)
 
-
-	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS
-	dj_make_short_path
+	mv $DJ_FNAME_TEMP $DJ_FNAME_ABS;
+	dj_make_short_path;
 }
- 
+
 
 function dj_next
 {
-	index_curr=0
+    index_curr=0;
 
-	dj_filter_real_path
-	dj_make_short_path
-	
-	while read line;
-	do
-		index=$(echo "$line" | awk '{print $1}')
-		directory=$(echo $line | awk '{print $2}')
+    dj_filter_real_path;
+    dj_make_short_path;
 
-		if [[ $PWD == $directory ]];then
-			index_curr=$index
-		fi
-	done < <(cat -n $DJ_FNAME_ABS)
+    while read line;
+    do
+        index=$(echo "$line" | awk '{print $1}')
+        directory=$(echo $line | awk '{print $2}')
 
-	index_curr=$(expr $index_curr + 1)
-	index_end=$(wc -l $DJ_FNAME_REL | awk '{print $1}')
+        if [[ $PWD == $directory ]];then
+            index_curr=$index;
+        fi
+    done < <(cat -n $DJ_FNAME_ABS)
 
-	#roll back
-	if [ $index_curr -gt $index_end ]; then
-		index_curr=1
-	fi
+    index_curr=$(expr $index_curr + 1);
+    index_end=$(wc -l $DJ_FNAME_REL | awk '{print $1}');
 
-	#change directory
-	dj_go $index_curr
-	return 0;
+    #roll back
+    if [ $index_curr -gt $index_end ]; then
+        index_curr=1;
+    fi
+
+    #change directory
+    dj_go $index_curr;
+    return 0;
 }
- 
+
 function dj_prev
 {
-	index_curr=0
+    index_curr=0;
 
-	dj_filter_real_path
-	dj_make_short_path
+    dj_filter_real_path;
+    dj_make_short_path;
 
-	while read line;
-	do
-		index=$(echo $line | awk '{print $1}')
-		directory=$(echo $line | awk '{print $2}')
+    while read line;
+    do
+        index=$(echo $line | awk '{print $1}');
+        directory=$(echo $line | awk '{print $2}');
 
-		if [[ $PWD == $directory ]];then
-			index_curr=$index
-		fi
+        if [[ $PWD == $directory ]];then
+            index_curr=$index;
+        fi
 
-	done < <(cat -n $DJ_FNAME_ABS)
+    done < <(cat -n $DJ_FNAME_ABS)
 
-	index_curr=$(expr $index_curr - 1)
-	index_end=$(wc -l $DJ_FNAME_REL | awk '{print $1}')
+    index_curr=$(expr $index_curr - 1);
+    index_end=$(wc -l $DJ_FNAME_REL | awk '{print $1}');
 
-	#roll back
-	if [ $index_curr -lt 1 ]; then
-		index_curr=$index_end
-	fi
+    #roll back
+    if [ $index_curr -lt 1 ]; then
+        index_curr=$index_end;
+    fi
 
-	#change directory
-	dj $index_curr
-	return 0;
+    #change directory
+    dj $index_curr;
+    return 0;
 }
 
 function dj_go
 {
-	# Check argument whether number.
-	if [ ! $(echo $1 | sed -n '/^[0-9]\+$/p') ]; then
-		dj_print_usage;
-		return;
-	fi
+    # Check argument whether number.
+    if [ ! $(echo $1 | sed -n '/^[0-9]\+$/p') ]; then
+        dj_print_usage;
+        return;
+    fi
 
-    DJ_TARGET=$(cat -n $DJ_FNAME_ABS | grep "^[[:space:]]*$1[[:space:]]" | awk '{print $2}')
+    DJ_TARGET=$(cat -n $DJ_FNAME_ABS | grep "^[[:space:]]*$1[[:space:]]" | awk '{print $2}');
 
-    cd $DJ_TARGET
+    cd $DJ_TARGET;
 }
 
 function dj
 {
-	if [ ! -f $DJ_FNAME_REL ]; then
-		touch $DJ_FNAME_REL
-	fi
-	if [ ! -f $DJ_FNAME_ABS ]; then
-		touch $DJ_FNAME_ABS
-	fi
+    if [ ! -f $DJ_FNAME_REL ]; then
+        touch $DJ_FNAME_REL
+    fi
+    if [ ! -f $DJ_FNAME_ABS ]; then
+        touch $DJ_FNAME_ABS
+    fi
 
 
-	case $1 in
+    case $1 in
 
-	"")
-		dj_dirs;
-		;;
+        "")
+            dj_dirs;
+            ;;
 
-	help)
-		dj_print_usage;
-		;;
-	add)
-		if [ $# -gt 2 ]; then
-			dj_print_usage;
-			return;
-		fi
-		if [[ $# == 1 ]]; then
-			dj_add "."
-		else
-			dj_add $2
-		fi
-		;;
-	rm)
-		if [ $# -gt 2 ]; then
-			dj_print_usage;
-			return;
-		elif [ $# -eq 1 ]; then
-			dj_rm_by_dirname "."
-		elif [ $# -eq 2 ]; then
-			dj_rm $2
-		fi
-		;;
-	next)
-		if [ $# -gt 1 ]; then
-			dj_print_usage;
-			return;
-		fi
-		dj_next $2;
-		;;
-	prev)
-		if [ $# -gt 1 ]; then
-			dj_print_usage;
-			return;
-		fi
-		dj_prev $2;
-		;;
-	clean)
-		if [ $# -gt 1 ]; then
-			dj_print_usage;
-			return;
-		fi
-		dj_clean;
-		;;
+    help)
+        dj_print_usage;
+        ;;
 
-	save)
-		if [ $# -gt 2 ]; then
-			dj_print_usage;
-			return;
-		fi
-		dj_save $2;
-		;;
-	load)
-		if [ $# -gt 2 ]; then
-			dj_print_usage;
-			return;
-		fi
-		dj_load $2;
-		;;
-	*)
-		dj_go $1
-		;;
-	esac
+    add)
+        if [ $# -gt 2 ]; then
+            dj_print_usage;
+            return;
+        fi
+        if [[ $# == 1 ]]; then
+            dj_add "."
+        else
+            dj_add $2
+        fi
+        ;;
+
+    rm)
+        if [ $# -gt 2 ]; then
+            dj_print_usage;
+            return;
+        elif [ $# -eq 1 ]; then
+            dj_rm_by_dirname ".";
+        elif [ $# -eq 2 ]; then
+            dj_rm $2;
+        fi
+        ;;
+
+    next)
+        if [ $# -gt 1 ]; then
+            dj_print_usage;
+            return;
+        fi
+        dj_next $2;
+        ;;
+
+    prev)
+        if [ $# -gt 1 ]; then
+            dj_print_usage;
+            return;
+        fi
+        dj_prev $2;
+        ;;
+
+    clean)
+        if [ $# -gt 1 ]; then
+            dj_print_usage;
+            return;
+        fi
+        dj_clean;
+        ;;
+
+    save)
+        if [ $# -gt 2 ]; then
+            dj_print_usage;
+            return;
+        fi
+        dj_save $2;
+        ;;
+
+    load)
+        if [ $# -gt 2 ]; then
+            dj_print_usage;
+            return;
+        fi
+        dj_load $2;
+        ;;
+
+    *)
+        dj_go $1
+        ;;
+        
+    esac
 }
